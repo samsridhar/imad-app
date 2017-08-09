7d1404d9baa5388b15e498e64b269d224b71924e
samsridhar <samsridhar.mac@gmail.com>
2017-08-09 23:03:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one':{
+        title: 'article-one',
+        heading: 'article-one',
+        date: 'sep 27,2017',
+        content: `<p>
+                 hi hello everybody welcome to spaceship.
+                 hi hello everybody welcome to spaceship.
+                 </p>
+               <p>
+                  hi hello everybody welcome to spaceship.
+                  hi hello everybody welcome to spaceship
+                  </p>`
+    },
+    'article-two':{
+         title: 'article-two',
+         heading: 'article-two',
+         date: 'sep 28,2017',
+         content: `<p>
+                 hi hello everybody welcome to spaceship on 28.
+                 hi hello everybody welcome to spaceship on 28.
+                 </p>
+               <p>
+                  hi hello everybody welcome to spaceship.
+                  hi hello everybody welcome to spaceship
+                  </p>`
+    },
+    'article-three':{
+         title: 'article-three',
+         heading: 'article-three',
+         date: 'sep 29,2017',
+         content: `<p>
+                 hi hello everybody welcome to spaceship 17.
+                 hi hello everybody welcome to spaceship 17.
+                 </p>
+               <p>
+                  hi hello everybody welcome to spaceship.
+                  hi hello everybody welcome to spaceship
+                  </p>`
+    }
+};
+
+var createTemplate = function(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = 
+   ` <html>
+  <head>
+    <title>
+        ${title}
+     </title>
+     <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+ <body>
+   <div class="container">
+         <div>
+            <a href ="/"> Home </a>
+            </div>
+         <hr/>
+         <h3>
+            ${heading}
+         </h3>
+         <div>
+            ${date}
+             </div>
+          <div>
+              ${content}
+            </div>    
+             
+   </div>
+  </body>
+  </html> `;
+    return htmlTemplate;
+};
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,16 +95,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+app.get('/:articleName', function(req, res){
+    var articleName = req.params.articleName;
+    res.sendFile(createTemplate(articles[articleName]));
 });
 
 
