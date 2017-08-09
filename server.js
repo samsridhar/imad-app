5ff3699a9ef946775b1f2085fee929e79e1afc3b
samsridhar <samsridhar.mac@gmail.com>
2017-08-09 23:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ var articles = {
     }
 };
 
-    function createTemplate (data){
+function createTemplate (data){
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
