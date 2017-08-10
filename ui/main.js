ffba7f51c090e23f586149facfc20cf5db12a2fb
samsridhar <samsridhar.mac@gmail.com>
2017-08-10 15:03:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var element = document.getElementById("main-text");
+element.innerhtml = "RAJINIKANTH";
+
+
+var img = document.getElementById("mandy");
+
+function rightmove(){
+    marginLeft = marginLeft+1;
+    img.style.marginLeft =marginLeft + 'px';
+}
+
+img.onClick = function(){
+    var interval = setInterval(rightmove, 50);
+};
