a378bbd70e5ffca6d650036acd927cd1b9cd1118
samsridhar <samsridhar.mac@gmail.com>
2017-08-10 17:45:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 var element = document.getElementById("sam");
-element.innerHtml = "changed";
+element.innerHtml = "kabali Dawwww.......nerupur dawwwww nerungu dawwwww...";
 
 
 var img = document.getElementById('madi');
