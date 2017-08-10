88251db07ca34695f2c650c687d6241fae1c12db
samsridhar <samsridhar.mac@gmail.com>
2017-08-10 15:12:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
