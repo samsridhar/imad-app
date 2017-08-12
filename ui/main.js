2840df7808aedd737748be5ebb5fc5e259ecdbb9
samsridhar <samsridhar.mac@gmail.com>
2017-08-12 19:40:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,21 +25,43 @@ request.onreadystatechange = function(){
 };
 
    //Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
       //Make a request to the server and send name
-      
-      //capture a list of names and render it as a list
-var names = ['name1', 'name2', 'name3'];
-var list = '';
+       //create a request
+ var request = new XMLHttpRequest();  
+ 
+ //capture the response and store it in a variable
+ 
+request.onreadystatechange = function(){
+    console.log("i am in");
+ if(request.readyState === XMLHttpRequest.DONE){
+     //Take some action
+     console.log("i am in");
+     if(request.status === 200){
+        var names = request.responseText;
+        names = JSON.parse(names);
+        var list = '';
 for(var i=0; i<names.length; i++){
     list += '<li>' + names[i] + '</li>';
 }
 
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
+     }
+     
+ }
+    //NOT done YET
+};
+     //Make a  request to the counter Endpoint
+     var nameInput = document.getElementById('name');
+     var name = nameInput.value;
+    request.open('GET','http://samsridharmac.imad.hasura-app.io/submit-name'+name,true);
+    request.send(null);
+};
+      //capture a list of names and render it as a list
+
       
       
 };
