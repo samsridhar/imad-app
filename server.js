821d702e29a18c0a74e97797ff240e07b9495f08
samsridhar <samsridhar.mac@gmail.com>
2017-08-12 19:14:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,9 +118,11 @@ app.get('/:articleName', function(req, res){
 var names = [];
 
 app.get('/submit-name/:name', function(req, res){
+    //Get the name from the request
    var name = req.params.name;
    names.push(name);
-   res.send(names);
+   //JSON javascript object notation
+   res.send(JSON.stringify(names));
 });
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
