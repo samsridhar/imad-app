6c2613728402cac0eafa2e4a4f72d6bec009c65d
samsridhar <samsridhar.mac@gmail.com>
2017-08-20 12:58:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/submit-name', function(req, res){ //URL: /submit-name?name=xxx
 
 app.get('/articles/:articleName', function(req, res){
 //SELECT * FROM article WHERE title = ''; DELETE WHERE a = 'asdf'
-pool.query("SELECT * FROM article WHERE title = $1", +[req.params.articleName], function(err, result){
+pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result){
     if(err){
         res.status(500).send(err.toString());
     }else{
