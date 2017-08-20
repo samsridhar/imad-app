690e45974699b6fa3d3e314c8b9deb97ab086341
samsridhar <samsridhar.mac@gmail.com>
2017-08-20 12:06:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,20 @@ app.get('/submit-name', function(req, res){ //URL: /submit-name?name=xxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
+//SELECT * FROM article WHERE title = 'article-one'    
+pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName + "'", function(err, result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length === 0){
+            res.status(400).send('Article Not Found');
+        }else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+});    
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
