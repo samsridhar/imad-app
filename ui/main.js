6a3037a1e23b384a46d033ad77a9a1c576d65404
samsridhar <samsridhar.mac@gmail.com>
2017-08-26 14:49:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,5 @@
-//counter code
-document.getElementById('click').onclick = function(){
-  //create a request
- var request = new XMLHttpRequest();  
- 
- //capture the response and store it in a variable
- 
-request.onreadystatechange = function(){
-    console.log("i am in");
- if(request.readyState === XMLHttpRequest.DONE){
-     //Take some action
-     console.log("i am in");
-     if(request.status === 200){
-         var counter = request.responseText;
-         console.log(counter);
-         document.getElementById('count').innerHTML = counter.toString();
-     }
-     
- }
-    //NOT done YET
-};
-     //Make a  request to the counter Endpoint
-    request.open('GET','http://samsridharmac.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
+//Submit UserName/Password to login
 
-   //Submit Name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
        //create a request
@@ -38,27 +13,26 @@ request.onreadystatechange = function(){
      //Take some action
      console.log("i am in");
      if(request.status === 200){
-        var names = request.responseText;
-        names = JSON.parse(names);
-        var list = '';
-for(var i=0; i<names.length; i++){
-    list += '<li>' + names[i] + '</li>';
-}
-
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+        console.log('User LoggedIN');
+        alert('user successfully logged in');
+     } else if(request.status === 403){
+         alert('Username/Password is not valid');
+     } else if (request.status === 500){
+         alert('something went wrong on the server');
      }
      
  }
     //NOT done YET
 };
      //Make a  request to the counter Endpoint
-     var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-    request.open('GET','http://samsridharmac.imad.hasura-app.io/submit-name?name='+ name,true);
-    request.send(null);
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+    request.open('POST','http://samsridharmac.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password:password}));
 };
-      //capture a list of names and render it as a list
-
+  
       
       
